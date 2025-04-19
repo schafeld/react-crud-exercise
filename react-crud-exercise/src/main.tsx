d081cba624ef59bc87f371587b140afb89968906
Oliver Schafeld <oliver@schafeld.com>
2025-04-19 16:02:10 +0200

    add basic router

--- react-crud-exercise/src/main.tsx
+++ react-crud-exercise/src/main.tsx
@@ -1,10 +1,27 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <div>Hello World</div>,
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
