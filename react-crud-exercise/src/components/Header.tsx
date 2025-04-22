4a132c6956e68ec375aa7ca4360f3dbeb5617be0
Oliver Schafeld <oliver@schafeld.com>
2025-04-22 17:17:25 +0200

    add home link to header title text

--- react-crud-exercise/src/components/Header.tsx
+++ react-crud-exercise/src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="flex space-x-4">
-      <h1 className="text-4xl font-bold text-gray-800 mt-6">CRUD App</h1>
+      <Link to="/">
+        <h1 className="text-4xl font-bold text-gray-800 mt-6">CRUD App</h1>
+      </Link>
     </header>
   )
 }
