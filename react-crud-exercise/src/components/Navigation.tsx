6d5ec9ddcdf08368aeb11505629c8a81618379de
Oliver Schafeld <oliver@schafeld.com>
2025-04-22 15:07:02 +0200

    underline active navigation links

--- react-crud-exercise/src/components/Navigation.tsx
+++ react-crud-exercise/src/components/Navigation.tsx
@@ -1,15 +1,73 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 export default function Navigation() {
   return (
-        <nav className="mt-4">
-          <Link to="/" className="mx-2 text-blue-500">Home</Link>
-          <Link to="/about" className="mx-2 text-blue-500">About</Link>
-          <Link to="/app" className="mx-2 text-blue-500">App</Link>
-          <Link to="/signin" className="mx-2 text-blue-500">Sign In</Link>
-          <Link to="/signup" className="mx-2 text-blue-500">Sign Up</Link>
-          <Link to="/forgot-password" className="mx-2 text-blue-500">Forgot Password</Link>
-          <Link to="/offers" className="mx-2 text-blue-500">Offers</Link>
-          <Link to="/profile" className="mx-2 text-blue-500">Profile</Link>
-        </nav>
+    <nav className="mt-4">
+      <NavLink 
+        to="/" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/about" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        About
+      </NavLink>
+      <NavLink 
+        to="/app" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        App
+      </NavLink>
+      <NavLink 
+        to="/signin" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        Sign In
+      </NavLink>
+      <NavLink 
+        to="/signup" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        Sign Up
+      </NavLink>
+      <NavLink 
+        to="/forgot-password" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        Forgot Password
+      </NavLink>
+      <NavLink 
+        to="/offers" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        Offers
+      </NavLink>
+      <NavLink 
+        to="/profile" 
+        className={({ isActive }) => 
+          isActive ? "mx-2 text-blue-500 underline" : "mx-2 text-blue-500"
+        }
+      >
+        Profile
+      </NavLink>
+    </nav>
   )
 }
