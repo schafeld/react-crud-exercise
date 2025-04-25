2aadfe61e605d9db5a43f1c8cdb896601caea33f
Oliver Schafeld <oliver@schafeld.com>
2025-04-25 20:17:08 +0200

    remove key

--- react-crud-exercise/src/firebase.ts
+++ react-crud-exercise/src/firebase.ts
@@ -13,13 +13,15 @@ interface FirebaseConfig {
 }
 
 const firebaseConfig: FirebaseConfig = {
+  // TODO: Connect API-Key with specific domain for deployment!
+  // Be absolutely sure to have DB rules set up to prevent unauthorized access.
   apiKey: '', // Add your Firebase API key here
   authDomain: 'react-crud-exercise.firebaseapp.com',
   projectId: 'react-crud-exercise',
   storageBucket: 'react-crud-exercise.firebasestorage.app',
   messagingSenderId: '321425804622',
   appId: '1:321425804622:web:ac907603e4372cfa90221f',
-  measurementId: 'G-ZNLFSWTCBD',
+  measurementId: "G-VMVZ168M8Q"
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
