8e60d817480ff25dae0939d0608bfeb200dcb33d
Oliver Schafeld <oliver@schafeld.com>
2025-05-02 11:24:28 +0200

    add create-listing link placeholder

--- react-crud-exercise/src/pages/Offers.tsx
+++ react-crud-exercise/src/pages/Offers.tsx
@@ -4,6 +4,17 @@ export default function Offers() {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 p-6">
       <h1 className="text-4xl font-bold text-gray-800 mt-6">Offers Page</h1>
+      <p className="text-gray-600 mt-4">
+        This is where you can view all the offers.
+      </p>
+
+
+        {/* If user is logged in show create listing button
+        <Link to="/create-listing" className="text-blue-500 mt-4">
+          Create Listing
+        </Link> */}
+
+
       <Link to="/about">About</Link>
       <Link to="/app">App</Link>
     </div>
