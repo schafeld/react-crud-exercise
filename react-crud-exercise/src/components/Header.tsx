2b46ca266c9d40090f82b3373c6f75cefedd40a7
Oliver Schafeld <oliver@schafeld.com>
2025-05-13 10:06:51 +0200

    remove odd border around header text

--- react-crud-exercise/src/components/Header.tsx
+++ react-crud-exercise/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="flex space-x-4">
+    <header>
       <Link to="/">
         <h1 className="text-4xl font-bold text-gray-800 mt-6">"Ollis Flohmarkt" CRUD App</h1>
       </Link>
